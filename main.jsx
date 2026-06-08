32d414f777d054c2134594c7f4b5919507675508
Frank Geusebroek <fgeusebroek@hotmail.com>
2026-06-08 16:50:15 +0200

    Refactor React imports and rendering method

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Wuyah from "./Wuyah.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Wuyah from './Wuyah.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <Wuyah />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
