6cd49cfdbe21066e9ba4af047815183e8fd8627e
Frank Geusebroek <fgeusebroek@hotmail.com>
2026-06-25 17:43:14 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Wuyah from './Wuyah.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Wuyah />
-  </StrictMode>,
-)
